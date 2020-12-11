3a4369c29bad682b8f22c47daa1f8d421ecc295c
praz <t.prazwal@gmail.com>
2020-12-11 13:21:42 +0100

    add button groups

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,16 +2,39 @@ import Button from './Button';
 
 const ButtonPanel = () => (
   <>
-    <Button button="9" />
-    <Button button="8" />
-    <Button button="7" />
-    <Button button="6" />
-    <Button button="5" />
-    <Button button="4" />
-    <Button button="3" />
-    <Button button="2" />
-    <Button button="1" />
-    <Button button="0" />
+    <div>
+      <Button button="AC" />
+      <Button button="+/-" />
+      <Button button="%" />
+      <Button button="/" />
+    </div>
+
+    <div>
+      <Button button="7" />
+      <Button button="8" />
+      <Button button="9" />
+      <Button button="x" />
+    </div>
+
+    <div>
+      <Button button="4" />
+      <Button button="5" />
+      <Button button="6" />
+      <Button button="-" />
+    </div>
+
+    <div>
+      <Button button="1" />
+      <Button button="2" />
+      <Button button="3" />
+      <Button button="+" />
+    </div>
+
+    <div>
+      <Button button="0" />
+      <Button button="." />
+      <Button button="=" />
+    </div>
   </>
 );
 
