c11834e0004fab5f74da0e59ae4be4af7ba5d82c
praz <t.prazwal@gmail.com>
2020-12-26 17:13:36 +0100

    add styling to the buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,32 @@
 import PropTypes from 'prop-types';
+import '../styles/Button.css';
 
-const Button = ({ button, handleClick }) => (
-  <button type="button" onClick={() => handleClick(button)}>{ button }</button>
+const Button = ({
+  button, handleClick, wide, color,
+}) => (
+  <button
+    className="button"
+    type="button"
+    style={{
+      width: wide ? '50%' : '25%',
+      backgroundColor: color,
+    }}
+    onClick={() => handleClick(button)}
+  >
+    { button }
+  </button>
 );
 
 Button.propTypes = {
   button: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired,
+  wide: PropTypes.bool,
+  color: PropTypes.string,
+};
+
+Button.defaultProps = {
+  wide: false,
+  color: '#E0E0E0',
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,40 +2,42 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 import '../styles/ButtonPanel.css';
 
+const color = '#F3913E';
+
 const ButtonPanel = ({ handleClick }) => (
   <>
     <div className="btnGroup">
       <Button button="AC" handleClick={handleClick} />
       <Button button="+/-" handleClick={handleClick} />
       <Button button="%" handleClick={handleClick} />
-      <Button button="÷" handleClick={handleClick} />
+      <Button button="÷" handleClick={handleClick} color={color} />
     </div>
 
     <div className="btnGroup">
       <Button button="7" handleClick={handleClick} />
       <Button button="8" handleClick={handleClick} />
       <Button button="9" handleClick={handleClick} />
-      <Button button="x" handleClick={handleClick} />
+      <Button button="x" handleClick={handleClick} color={color} />
     </div>
 
     <div className="btnGroup">
       <Button button="4" handleClick={handleClick} />
       <Button button="5" handleClick={handleClick} />
       <Button button="6" handleClick={handleClick} />
-      <Button button="-" handleClick={handleClick} />
+      <Button button="-" handleClick={handleClick} color={color} />
     </div>
 
     <div className="btnGroup">
       <Button button="1" handleClick={handleClick} />
       <Button button="2" handleClick={handleClick} />
       <Button button="3" handleClick={handleClick} />
-      <Button button="+" handleClick={handleClick} />
+      <Button button="+" handleClick={handleClick} color={color} />
     </div>
 
     <div className="btnGroup">
-      <Button button="0" handleClick={handleClick} />
+      <Button button="0" handleClick={handleClick} wide />
       <Button button="." handleClick={handleClick} />
-      <Button button="=" handleClick={handleClick} />
+      <Button button="=" handleClick={handleClick} color={color} />
     </div>
   </>
 );
