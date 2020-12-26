01c618ece006d424433d679624195643f0c3d930
praz <t.prazwal@gmail.com>
2020-12-26 16:04:18 +0100

    add style to ButtonPanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,37 +1,38 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
+import '../styles/ButtonPanel.css';
 
 const ButtonPanel = ({ handleClick }) => (
   <>
-    <div>
+    <div className="btnGroup">
       <Button button="AC" handleClick={handleClick} />
       <Button button="+/-" handleClick={handleClick} />
       <Button button="%" handleClick={handleClick} />
       <Button button="÷" handleClick={handleClick} />
     </div>
 
-    <div>
+    <div className="btnGroup">
       <Button button="7" handleClick={handleClick} />
       <Button button="8" handleClick={handleClick} />
       <Button button="9" handleClick={handleClick} />
       <Button button="x" handleClick={handleClick} />
     </div>
 
-    <div>
+    <div className="btnGroup">
       <Button button="4" handleClick={handleClick} />
       <Button button="5" handleClick={handleClick} />
       <Button button="6" handleClick={handleClick} />
       <Button button="-" handleClick={handleClick} />
     </div>
 
-    <div>
+    <div className="btnGroup">
       <Button button="1" handleClick={handleClick} />
       <Button button="2" handleClick={handleClick} />
       <Button button="3" handleClick={handleClick} />
       <Button button="+" handleClick={handleClick} />
     </div>
 
-    <div>
+    <div className="btnGroup">
       <Button button="0" handleClick={handleClick} />
       <Button button="." handleClick={handleClick} />
       <Button button="=" handleClick={handleClick} />
