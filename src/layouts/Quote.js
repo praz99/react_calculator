4601a91782511b44954bcf2ab983636c693cc297
praz <t.prazwal@gmail.com>
2020-12-27 18:58:28 +0100

    minor style changes

--- src/layouts/Quote.js
+++ src/layouts/Quote.js
@@ -3,7 +3,7 @@ import '../styles/Quote.css';
 
 const Quote = () => (
   <div className="quote">
-    <p>Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul Thurston</p>
+    <p className="quote-para">Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul Thurston</p>
   </div>
 );
 
