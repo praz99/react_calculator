b36c2a1ca0215cedf88c183284871169244b3595
praz <t.prazwal@gmail.com>
2020-12-27 18:23:07 +0100

    add contnet and style to home page

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,5 +1,20 @@
+/* eslint-disable max-len */
+
+import React from 'react';
+import '../styles/Home.css';
+
 const Home = () => (
-  <h1>This is the Home</h1>
+  <div className="home">
+    <h2>Welcome to our page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui o!cia deserunt mollit anim id est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui o!cia deserunt mollit anim id est laborum.
+    </p>
+  </div>
 );
 
 export default Home;
+
+/* eslint-enable max-len */
