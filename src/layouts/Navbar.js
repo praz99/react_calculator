8f65bf648c49dfc6cb49f43a16a265b6dd0d7f85
praz <t.prazwal@gmail.com>
2020-12-27 18:07:42 +0100

    add style to navbar

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import '../styles/Navbar.css';
 
 const Navbar = () => (
   <div className="navbar">
+    <h1>Math Magicians</h1>
     <ul>
       <li>
         <a href="/">Home</a>
